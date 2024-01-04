eb555a0e33e1fdddc9e87a2b1525ef8572b798d4
misulik112 <ursu.alin89@gmail.com>
2024-01-04 20:32:40 +0200

    Adding the setup script

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Enable shell script strictness
+set -eu
+
+# Enable command tracing
+set -x
+
+# Ensure config directory exists
+mkdir -p ~/.config
+
+# Link Git config if it doesn't exists
+[ ! -e ~/.config/git ] && ln -s "$PWD/config/git" ~/.config/git
+
+# Link Neovim config if it doesn't exists
+#[ ! -e ~/.config/nvim ] && ln -s "$PWD/config/nvim" ~/.config/nvim
+
+# Link Xmodmap configuration
+[ ! -e ~/.Xmodmap ] && ln -s "$PWD/config/Xmodmap" ~/.Xmodmap
+
+# Link Alacritty config if it doesn't exists
+[ ! -e ~/.config/alacritty ] && ln -s "$PWD/config/alacritty" ~/.config/alacritty
+
+# Link ZSH config if it doesn't exists
+[ ! -e ~/.zshrc ] && ln -s "$PWD/config/zshrc" ~/.zshrc
