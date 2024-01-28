4c01d9e9fca7d93655f0eae01cf50bc1afc8c56e
misulik112 <ursu.alin89@gmail.com>
2024-01-28 16:27:11 +0200

    Adding starship, bashrc and install packages
    Installing and configuring starship, basrc and a script to install packages after a fresh install.

---
+++ config/install_packages.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Install packages
+sudo pacman -S nerd-fonts nodejs npm starship
--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ mkdir -p ~/.config
 [ ! -e ~/.config/git ] && ln -s "$PWD/config/git" ~/.config/git
 
 # Link Git config if it doesn't exists
-[ ! -e ~/.config/qtile] && ln -s "$PWD/config/qtile" ~/.config/qtile
+[ ! -e ~/.config/qtile ] && ln -s "$PWD/config/qtile" ~/.config/qtile
 
 # Link Neovim config if it doesn't exists
 [ ! -e ~/.config/nvim ] && ln -s "$PWD/config/nvim" ~/.config/nvim
@@ -23,6 +23,12 @@ mkdir -p ~/.config
 
 # Link Alacritty config if it doesn't exists
 [ ! -e ~/.config/alacritty ] && ln -s "$PWD/config/alacritty" ~/.config/alacritty
+
+# Link starship config if it doesn't exists
+[ ! -e ~/.config/starship.toml ] && ln -s "$PWD/config/starship/starship.toml" ~/.config/starship.toml
+
+# Link bash config if it doesn't exists
+[ ! -e ~/.bashrc ] && ln -s "$PWD/config/bashrc" ~/.bashrc
 
 # Link ZSH config if it doesn't exists
 #[ ! -e ~/.zshrc ] && ln -s "$PWD/config/zshrc" ~/.zshrc
