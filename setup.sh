cf94d737dc130ca9efd8111ac3986190648d23d1
misulik112 <ursu.alin89@gmail.com>
2024-01-28 15:18:57 +0200

    Uncommenting qtile and alacritty
    Uncommenting the lines for qtile and alacritty configurations.

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ mkdir -p ~/.config
 [ ! -e ~/.config/git ] && ln -s "$PWD/config/git" ~/.config/git
 
 # Link Git config if it doesn't exists
-#[ ! -e ~/.config/qtile] && ln -s "$PWD/config/qtile" ~/.config/qtile
+[ ! -e ~/.config/qtile] && ln -s "$PWD/config/qtile" ~/.config/qtile
 
 # Link Neovim config if it doesn't exists
 [ ! -e ~/.config/nvim ] && ln -s "$PWD/config/nvim" ~/.config/nvim
@@ -22,7 +22,7 @@ mkdir -p ~/.config
 [ ! -e ~/.Xmodmap ] && ln -s "$PWD/config/Xmodmap" ~/.Xmodmap
 
 # Link Alacritty config if it doesn't exists
-#[ ! -e ~/.config/alacritty ] && ln -s "$PWD/config/alacritty" ~/.config/alacritty
+[ ! -e ~/.config/alacritty ] && ln -s "$PWD/config/alacritty" ~/.config/alacritty
 
 # Link ZSH config if it doesn't exists
 #[ ! -e ~/.zshrc ] && ln -s "$PWD/config/zshrc" ~/.zshrc
